ba1e3fbf2e5f7f1cbf29a2f7935272e37044106a
Niclas Lövdahl <niclas.lovdahl@gmail.com>
2018-11-30 19:05:45 +0100

    reformated variables according to Googles styleguide.

--- 3.2/script.js
+++ 3.2/script.js
@@ -2,17 +2,17 @@
  * This function gets and sets values and attributes of a link-tag. It also
  * updates a paragraph and removes a button.
  */
-$("#linkButton").click(() => {
+$("#link-button").click(() => {
     $(".link-google").removeClass("link-google").addClass("link-youtube");
     $(".link-youtube").text("Nu leder länken till YouTube");
     $(".link-youtube").attr("href", "https://youtube.com");
-    $(".infoText").text("Knappen du tryckte på har id:" + $("#linkButton").attr("id"));
-    $("#linkButton").remove();
+    $(".info-text").text("Knappen du tryckte på har id:" + $("#link-button").attr("id"));
+    $("#link-button").remove();
 });
 
 /**
  * This function updates a paragraph with the current width of the browser.
  */
 $(window).resize(() => {
-    $(".windowText").text("Webbläsarens bredd är " + $(window).width() + " pixlar.");
+    $(".window-text").text("Webbläsarens bredd är " + $(window).width() + " pixlar.");
 });
